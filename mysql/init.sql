f2b3051c56f5ad3607a324c1cdedd144870d1cac
ゆういち <159900048+yuichi-star-alchemist@users.noreply.github.com>
2024-08-08 23:35:15 +0900

    [fix] 初期挿入データの日付をDate型に修正

--- mysql/init.sql
+++ mysql/init.sql
@@ -22,6 +22,6 @@ INSERT INTO users (user_id, user_name, email, password, user_address)
            VALUES (1, 'name1', 'sample@email.com1', 'password1', 'address1'),
                   (2, 'name2', 'sample@email.com2', 'password2', 'address2');
 INSERT INTO schedules (user_id, date, destination, destination_address)
-               VALUES (1, '2024,8,31', 'ディズニーランド', '千葉県浦安市舞浜１−１'),
-                      (1, '2024,10,5', '東京タワー', '東京都港区芝公園４丁目２−８'),
-                      (2, '2024,9,20', '阪神甲子園球場', '兵庫県西宮市甲子園町１−８２');
+               VALUES (1, '2024-08-31', 'ディズニーランド', '千葉県浦安市舞浜１−１'),
+                      (1, '2024-10-05', '東京タワー', '東京都港区芝公園４丁目２−８'),
+                      (2, '2024-09-20', '阪神甲子園球場', '兵庫県西宮市甲子園町１−８２');
